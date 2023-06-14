596a0a03bbde83f615889ba6326c8284a0b21989
Stiven Medina <stvnm33@gmail.com>
2023-06-13 22:16:24 -0500

    Añadiendo la intefaz grafica en pages/Ventas.tsx

--- src/pages/Ventas.tsx
+++ src/pages/Ventas.tsx
@@ -1,7 +1,268 @@
+import Card from '../components/Card'
+import './ventas.css'
+import InputSpinner from '../components/InputSpinner'
+
 const Ventas = () => {
-  return (
-    <div>Ventas</div>
-  )
+    return (
+        <>
+            <Card>
+                <header>Ventas</header>
+                <div className="contenedor">
+                    <div className="derecha">
+
+                        <div className="main">
+
+                            <div className="main-left">
+                                <div className="input-fields">
+                                    <label htmlFor="inicioRuta">Agencia</label>
+                                    <input
+                                        type="text"
+                                        id="inicioRuta"
+                                        placeholder="Ingrese el inicio de la ruta"
+
+                                    />
+
+                                </div>
+
+                                <div className="input-fields">
+                                    <label htmlFor="inicioRuta">Ruta</label>
+                                    <input
+                                        type="text"
+                                        id="inicioRuta"
+                                        placeholder="Ingrese el inicio de la ruta"
+
+                                    />
+
+                                </div>
+
+                                <div className="input-fields">
+                                    <label htmlFor="inicioRuta">Vehiculo</label>
+                                    <input
+                                        type="text"
+                                        id="inicioRuta"
+                                        placeholder="Ingrese el inicio de la ruta"
+
+                                    />
+
+                                </div>
+
+                                <div className="input-fields">
+                                    <label htmlFor="inicioRuta">Conductor</label>
+                                    <input
+                                        type="text"
+                                        id="inicioRuta"
+                                        placeholder="Ingrese el inicio de la ruta"
+
+                                    />
+
+                                </div>
+
+                                <div className="input-fields">
+                                    <label htmlFor="inicioRuta">Conductor</label>
+                                    <input
+                                        type="text"
+                                        id="inicioRuta"
+                                        placeholder="Ingrese el inicio de la ruta"
+
+                                    />
+
+                                </div>
+                            </div>
+
+
+                            <div className="main-right">
+                                <div className="input-fields">
+                                    <label htmlFor="inicioRuta">Fecha</label>
+                                    <input
+                                        type="text"
+                                        id="inicioRuta"
+                                        placeholder="Ingrese el inicio de la ruta"
+
+                                    />
+
+                                </div>
+
+                                <div className="input-fields">
+                                    <label htmlFor="inicioRuta">Hora</label>
+                                    <input
+                                        type="text"
+                                        id="inicioRuta"
+                                        placeholder="Ingrese el inicio de la ruta"
+
+                                    />
+
+                                </div>
+
+                                <div className="input-fields">
+                                    <label htmlFor="inicioRuta">P Ocupados</label>
+                                    <input
+                                        type="text"
+                                        id="inicioRuta"
+                                        placeholder="Ingrese el inicio de la ruta"
+
+                                    />
+
+                                </div>
+
+                                <div className="input-fields">
+                                    <label htmlFor="inicioRuta">Valor tiquete</label>
+                                    <input
+                                        type="text"
+                                        id="inicioRuta"
+                                        placeholder="Ingrese el inicio de la ruta"
+
+                                    />
+
+                                </div>
+
+                                <label htmlFor="">Cantidad puestos</label>
+
+                                <InputSpinner/>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="izquierda">
+                        <div className="planilla seleccionado">
+                            <div className="planilla-info">
+                                <span className="ruta">TEST - TEST</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado pendiente">Pendiente</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">TIMBIO - POPAYAN</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">EL BORDO - ROSAS</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">CALI - EL TAMBO</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">CALI - EL TAMBO</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">CALI - EL TAMBO</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">CALI - EL TAMBO</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">CALI - EL TAMBO</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">CALI - EL TAMBO</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">CALI - EL TAMBO</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">CALI - EL TAMBO</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+                        <div className="planilla">
+                            <div className="planilla-info">
+                                <span className="ruta">CALI - EL TAMBO</span>
+                                <span className="vehiculo">Vehiculo: 3060</span>
+                            </div>
+
+                            <div className="planilla_estado">
+                                <span className="estado despachado">Despachado</span>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </Card>
+        </>
+    )
 }
 
 export default Ventas
