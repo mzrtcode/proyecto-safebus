7f5587fdb1183d10d90606d3545fe6a5139f67f6
Stiven Medina <stvnm33@gmail.com>
2023-09-04 11:25:49 -0500

    Añadiendo archivo para peticiones a la API para planillaje

--- src/api/planillaje.ts
+++ src/api/planillaje.ts
@@ -1,10 +1,32 @@
+import axios from './axios';
+
 export interface PlanillajeTypes {
-    id_planillaje: number
-    id_ruta: number
-    id_conductor: number
-    id_vendeodr: number
-    id_vehiculo: number
-    id_agencia: number
-    viaje_completado: boolean
-    hora_salida: Date
+  id_planilla: number;
+  inicio_ruta: string;
+  fin_ruta: string;
+  nombre_conductor: string;
+  apellido_conductor: string;
+  nombre_vendedor: string;
+  apellido_vendedor: string;
+  numero_placa_vehiculo: string;
+  codigo_interno_vehiculo: string;
+  nombre_agencia: string;
+  hora_salida: Date | null;
+  viaje_completado: boolean
+}
+
+
+export const planillajeLoader = async (): Promise<PlanillajeTypes[]> => {
+  try {
+    const res = await axios.get("/planillas");
+    if (res.status !== 200) {
+      throw new Error("Error al obtener los datos de planillas");
+    }
+    return res.data.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    }
+    return []; // Devuelve un array vacío en caso de error.
   }
+};
