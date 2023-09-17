2b1a9696a05d2a1bf9c6a0b481f518e787debe3c
Stiven Medina <stvnm33@gmail.com>
2023-09-16 21:36:52 -0500

    Añadiendo funcion que retorna fecha y hora con el formato mm/AAAA mm:HH

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,20 @@ export function formatoHoraAmPm(fechaHora: Date): string {
     const horaFormateada: string = `${hora12}:${minutos < 10 ? '0' : ''}${minutos} ${amPm}`;
     return horaFormateada;
 }
+
+export function obtenerFechaYHoraActual(): string {
+    const ahora = new Date();
+    const mes = ahora.getMonth() + 1; // Los meses comienzan desde 0
+    const anio = ahora.getFullYear();
+    const horas = ahora.getHours();
+    const minutos = ahora.getMinutes();
+  
+    // Formatea el mes y los minutos para que tengan dos dígitos
+    const mesFormateado = mes < 10 ? `0${mes}` : mes;
+    const minutosFormateados = minutos < 10 ? `0${minutos}` : minutos;
+  
+    // Crea la cadena de fecha y hora en el formato mm/aaaa hh:mm
+    const fechaYHora = `${mesFormateado}/${anio} ${horas}:${minutosFormateados}`;
+  
+    return fechaYHora;
+  }
