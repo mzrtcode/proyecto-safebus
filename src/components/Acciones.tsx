2a6e06645c6bbec514b5871144b92ebf4420574c
Stiven Medina <stvnm33@gmail.com>
2023-11-30 12:04:37 -0500

    Modificando la logica del componente Acciones ahora si se le pasa null en el link de editar no renderiza el boton de editar

--- src/components/Acciones.tsx
+++ src/components/Acciones.tsx
@@ -2,26 +2,28 @@ import { Link, useNavigate } from 'react-router-dom'
 import styles from './acciones.module.css'
 
 interface AccionesProps {
-  editarLink: string;
+  editarLink: string | null;
   eliminar: (id: number) => void;
   id: number;
 }
-
 
 const Acciones: React.FC<AccionesProps> = ({ editarLink, eliminar, id }) => {
   const navigate = useNavigate();
 
   return (
     <div className={styles.acciones}>
-      <span className={styles.editar}>
-      <i className='bx bxs-pencil' onClick={()=> navigate(editarLink)} /> 
-      </span>
-      <span className={styles.eliminar} onClick={()=>{eliminar(id)}}>
+      {editarLink !== null && (
+        <span className={styles.editar}>
+          <i className='bx bxs-pencil' onClick={() => navigate(editarLink)} />
+        </span>
+      )}
+      <span className={styles.eliminar} onClick={() => eliminar(id)}>
         <i className='bx bx-minus-circle' />
       </span>
-
     </div>
   )
 }
-export default Acciones
+
+export default Acciones;
+
 
